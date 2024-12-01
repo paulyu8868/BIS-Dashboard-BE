34159bb2e554ae0cff00355e77d49a6e50016497
paul <py4123285@naver.com>
2024-12-01 20:51:36 +0900

    feat: 1) RouteService에 정류장 조회,버텍스 조회 추가 2) VertexDTO, BusStopDTO 추가 3) RouteCotroller에 정류장조회,버텍스 조회 api 추가

--- src/main/java/com/example/bis/simulator/controller/RouteController.java
+++ src/main/java/com/example/bis/simulator/controller/RouteController.java
@@ -1,10 +1,14 @@
 package com.example.bis.simulator.controller;
 
+import com.example.bis.simulator.dto.BusStopDTO;
 import com.example.bis.simulator.dto.RouteData;
+import com.example.bis.simulator.dto.VertexDTO;
 import com.example.bis.simulator.service.RouteService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/route")
@@ -21,4 +25,40 @@ public class RouteController {
     public List<RouteData> getTop10Routes() {
         return routeService.getOptimizedRouteData();
     }
+
+
+    // 정류장 조회 엔드포인트
+    @GetMapping("/busstops/{routeId}")
+    public ResponseEntity<List<BusStopDTO>> getBusStops(@PathVariable String routeId) {
+        try {
+            List<BusStopDTO> busStops = routeService.getBusStops(routeId);
+
+            if (busStops.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(busStops);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+
+    // 노선의 모든 버텍스 위치 조회 (순서 정렬 완료)
+    @GetMapping("/vertices/{routeId}")
+    public ResponseEntity<List<VertexDTO>> getRouteVertexes(@PathVariable String routeId) {
+        try {
+            List<VertexDTO> vertexes = routeService.getRouteVertexes(routeId);
+
+            if (vertexes.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(vertexes);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+
 }
